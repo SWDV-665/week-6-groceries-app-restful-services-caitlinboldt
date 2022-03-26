ea5e6256d99d1ea936b44706e260bff359d4f1c7
Caitlin Boldt <caitlin.boldt@freeman.com>
2022-03-26 16:53:07 -0700

    Add and remove items from the grocery list

--- src/app/grocery-list/grocery-list.page.ts
+++ src/app/grocery-list/grocery-list.page.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 
 export interface GroceryProps {
-  id: number;
   name: string;
   cost: number;
   quantity: number;
@@ -15,16 +16,59 @@ export interface GroceryProps {
 
 export class GroceryListPage {
 
-  groceries: GroceryProps[] = [
-    {id: 1, name: 'Eggs', cost: 4, quantity: 2},
-    {id: 2, name: 'Chicken thighs', cost: 12, quantity: 3},
-    {id: 3, name: 'Bacon', cost: 5, quantity: 4},
-    {id: 4, name: 'Bread', cost: 3, quantity: 1},
-    {id: 5, name: 'Cabbage', cost: 1, quantity: 1},
-    {id: 6, name: 'Yogurt', cost: 1, quantity: 12}
-  ];
+  groceries: GroceryProps[] = [];
 
-  removeGroceryItem(index) {
+  constructor(public toastCtrl: ToastController, public alertController: AlertController) {
+  }
+
+  async addGroceryItem() {
+    const alert = await this.alertController.create({
+      header: 'Add a Grocery Item',
+      inputs: [
+        {
+          name: 'name',
+          type: 'text',
+          placeholder: 'Name of grocery item'
+        },
+        {
+          name: 'cost',
+          type: 'number',
+          min: 0,
+          placeholder: 'Cost of grocery item'
+        },
+        {
+          name: 'quantity',
+          type: 'number',
+          min: 0,
+          placeholder: 'Quantity'
+        }
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: () => {
+            console.log('Confirm Cancel');
+          }
+        }, {
+          text: 'Add',
+          handler: (data) => {
+            this.groceries.push(data);
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+  async removeGroceryItem(index) {
+    const toast = await this.toastCtrl.create({
+      message: `Removing ${this.groceries[index].name} from the grocery list.`,
+      duration: 3000
+    });
+    toast.present();
+
     if (this.groceries.length > 1) {
       this.groceries.splice(index, 1);
     } else {
